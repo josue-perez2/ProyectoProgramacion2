b58c0bec2015c8552347c9c0205231a478a3d652
jperez-2022076 <jperez-2022076@kinal.edu.gt>
2026-09-20 14:14:11 -0600

    Conexion a la base de datos

--- src/config/Conexion.java
+++ src/config/Conexion.java
@@ -1,4 +1,31 @@
 package config;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 
 public class Conexion {
+
+    public Connection conectar()  {
+        // Si van a cambiar las credenciales para la base de datos no lo suban
+        // que cada quiern lo cambie cuando tengan que trabajar
+        Connection conexion = null;
+        String url = "jdbc:oracle:thin:@localhost:1521:XE";
+        String usuario = "SYSTEM";
+        String password = "Administrador1";
+
+        try {
+            conexion = DriverManager.getConnection(
+                    url,
+                    usuario,
+                    password
+            );
+            System.out.println("Si se pudo");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return conexion;
+    }
+
 }
